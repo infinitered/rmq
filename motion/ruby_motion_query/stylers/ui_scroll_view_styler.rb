6c4ccab62c9ae56ca238c1dd39a14d079500c8f3
Mark Rickert <mjar81@gmail.com>
2014-02-22 09:36:33 -0500

    Add ability to turn scrolling on and off in a scrollview styler.

--- motion/ruby_motion_query/stylers/ui_scroll_view_styler.rb
+++ motion/ruby_motion_query/stylers/ui_scroll_view_styler.rb
@@ -1,9 +1,13 @@
 module RubyMotionQuery
   module Stylers
 
-    class UIScrollViewStyler < UIViewStyler 
+    class UIScrollViewStyler < UIViewStyler
       def paging=(value) ; @view.pagingEnabled = value ; end
       def paging ; @view.isPagingEnabled ; end
+
+      def scroll_enabled=(value) ; @view.scrollEnabled = value ; end
+      def scroll_enabled? ; @view.isScrollEnabled ; end
+
     end
 
   end
