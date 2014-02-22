cb12ee4f88e8c66994f0d579c9f2a99639bc6412
Mark Rickert <mjar81@gmail.com>
2014-02-22 09:42:23 -0500

    Remove ? from methods.

--- motion/ruby_motion_query/stylers/ui_scroll_view_styler.rb
+++ motion/ruby_motion_query/stylers/ui_scroll_view_styler.rb
@@ -6,10 +6,10 @@ module RubyMotionQuery
       def paging ; @view.isPagingEnabled ; end
 
       def scroll_enabled=(value) ; @view.scrollEnabled = value ; end
-      def scroll_enabled? ; @view.isScrollEnabled ; end
+      def scroll_enabled ; @view.isScrollEnabled ; end
 
       def direction_lock=(value) ; @view.directionalLockEnabled = value ; end
-      def direction_lock? ; @view.isDirectionalLockEnabled ; end
+      def direction_lock ; @view.isDirectionalLockEnabled ; end
 
     end
 
