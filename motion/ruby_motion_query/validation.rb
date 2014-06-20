2521fd70e33e4aedca4125c03a220b742c48c168
Gant <GantMan@gmail.com>
2014-06-20 09:49:44 -0500

    added more validations

--- motion/ruby_motion_query/validation.rb
+++ motion/ruby_motion_query/validation.rb
@@ -19,13 +19,25 @@ module RubyMotionQuery
       DATEISO = Regexp.new('^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$')
       NUMBER = Regexp.new('^-?(?:\d+|\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$')
       DIGITS = Regexp.new('^\d+$')
+      # Other Fun by http://www.freeformatter.com/regex-tester.html
+      IPV4 = Regexp.new('^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
+      TIME = Regexp.new('^(20|21|22|23|[01]\d|\d)((:[0-5]\d){1,2})$')
+      # Future Password strength validations -> http://stackoverflow.com/questions/5142103/regex-for-password-strength
+      USZIP = Regexp.new('^\d{5}(-\d{4})?$')
+      # 7 or 10 digit number, delimiters are spaces, dashes, or periods
+      USPHONE = Regexp.new('^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]‌​)\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-‌​9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})$')
+
 
       @@validation_methods = {
         :email => lambda { |value| Validation.regex_match?(value, EMAIL)},
         :url => lambda { |value| Validation.regex_match?(value, URL)},
         :dateiso => lambda { |value| Validation.regex_match?(value, DATEISO)},
         :number => lambda { |value| Validation.regex_match?(value, NUMBER)},
-        :digits => lambda { |value| Validation.regex_match?(value, DIGITS)}
+        :digits => lambda { |value| Validation.regex_match?(value, DIGITS)},
+        :ipv4 => lambda { |value| Validation.regex_match?(value, IPV4)},
+        :time => lambda { |value| Validation.regex_match?(value, TIME)},
+        :uszip => lambda { |value| Validation.regex_match?(value, USZIP)},
+        :usphone => lambda { |value| Validation.regex_match?(value, USPHONE)}
       }
 
       # Add tags
