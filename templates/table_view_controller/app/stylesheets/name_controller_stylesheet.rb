2cc0644fd7c40965269e8a7aede1391e8cdb4c2d
Todd Werth <todd@infinitered.com>
2014-12-12 14:12:38 -0800

    Updated table template to make it simpler, no need for extra stylesheet for the view, it just makes it overly complicated

--- templates/table_view_controller/app/stylesheets/name_controller_stylesheet.rb
+++ templates/table_view_controller/app/stylesheets/name_controller_stylesheet.rb
@@ -1,7 +1,5 @@
 class <%= @name_camel_case %>ControllerStylesheet < ApplicationStylesheet
 
-  include <%= @name_camel_case %>CellStylesheet
-
   def setup
     # Add stylesheet specific setup stuff here.
     # Add application specific setup stuff in application_stylesheet.rb
@@ -9,5 +7,18 @@ class <%= @name_camel_case %>ControllerStylesheet < ApplicationStylesheet
 
   def table(st)
     st.background_color = color.gray
+  end
+
+  def <%= @name %>_cell_height
+    80
+  end
+
+  def <%= @name %>_cell(st)
+    # Style overall cell here
+    st.background_color = color.random
+  end
+
+  def cell_label(st)
+    st.color = color.black
   end
 end
