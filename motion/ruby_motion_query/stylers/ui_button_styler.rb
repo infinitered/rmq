87e052338154bd3895dded583946200cf5f3b049
Frank von Hoven <fvonhoven@gmail.com>
2015-01-20 16:46:24 -0600

    Added text_highlighted styler

--- motion/ruby_motion_query/stylers/ui_button_styler.rb
+++ motion/ruby_motion_query/stylers/ui_button_styler.rb
@@ -78,6 +78,14 @@ module RubyMotionQuery
         @view.imageEdgeInsets
       end
 
+      def text_highlighted value
+        @view.setTitle(value, forState:UIControlStateHighlighted)
+      end
+
+      def text_highlighted= value
+        @view.setTitle(value, forState:UIControlStateHighlighted)
+      end
+
     end
   end
 end
