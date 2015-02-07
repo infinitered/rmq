8741f90dc6e3de87a04a43d75861785866686683
Gant <GantMan@gmail.com>
2015-02-07 01:03:29 -0600

    adding table cell stylers

--- motion/ruby_motion_query/stylers/ui_table_view_cell_styler.rb
+++ motion/ruby_motion_query/stylers/ui_table_view_cell_styler.rb
@@ -19,6 +19,17 @@ module RubyMotionQuery
         @view.accessoryView
       end
 
+      def text_color ; @view.textLabel.textColor ; end
+      def text_color=(v) ; @view.textLabel.textColor = v ; end
+      alias :color :text_color
+      alias :color= :text_color=
+
+      def font ; @view.textLabel.font ; end
+      def font=(v) ; @view.textLabel.font = v ; end
+
+      def selection_style ; @view.selectionStyle ; end
+      def selection_style=(v) ; @view.selectionStyle = v ; end
+
     end
 
   end
